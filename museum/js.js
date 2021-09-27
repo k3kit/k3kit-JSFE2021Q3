b8efac15a3836e87dd6a227c4be57b9567784a7b
Nikita berezkin <hadouken7809@gmail.com>
2021-09-27 21:25:04 +0300

    fix: fix section contact

--- museum/js.js
+++ museum/js.js
@@ -24,13 +24,13 @@
 //     return img;
 //   }
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiaGFkb3VrZW43ODA5IiwiYSI6ImNrdTFpYjlyYzNsMm4yb25xOGppbG1mZXUifQ.nBk1rPwLCoG9aww_BJRBOA';
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFkb3VrZW43ODA5IiwiYSI6ImNrdTFpOWVvNTI4NW8ybnA4cWdvbWR5cWkifQ.CMpP6WjNcKAXGQAGjt5SzQ';
 
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/light-v10',
+  style: 'mapbox://styles/hadouken7809/cku2zajbw064917lfu0i0l2z8',
   center: [2.33587, 48.86102],
-  zoom: 16
+  zoom: 15.5
 });
 
 map.addControl(new mapboxgl.NavigationControl());
