65498fc72a59abfe665020b1181a07bb74015622
Nikita berezkin <hadouken7809@gmail.com>
2021-10-05 14:49:24 +0300

    fix

---
+++ museum-adaptive/gallery.js
@@ -0,0 +1,30 @@
+const pictureInnerContainer = document.querySelector(
+    ".picture-inner-container",
+  );
+  function createGallery(elem, amount) {
+    const images = [];
+    let count = 1;
+  
+    while (count <= amount) {
+      images.push(createImg(count));
+      count++;
+    }
+  
+    images.sort(() => 0.5 - Math.random());
+  
+    elem.append(...images);
+  }
+  
+  function createImg(number) {
+    const picture = document.createElement("picture");
+    const img = document.createElement("img");
+    img.src = `assets/img/galery/webp/galery${number}.webp`;
+    img.classList.add("picture-container__picture");
+    img.alt = `galery${number}`;
+    img.loading = "lazy";
+    picture.append(img);
+    return img;
+  }
+  
+  
+  createGallery(pictureInnerContainer, 15);
---
+++ museum-adaptive/map.js
@@ -0,0 +1,11 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFkb3VrZW43ODA5IiwiYSI6ImNrdTFpOWVvNTI4NW8ybnA4cWdvbWR5cWkifQ.CMpP6WjNcKAXGQAGjt5SzQ';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/hadouken7809/cku2zajbw064917lfu0i0l2z8',
+  center: [2.33587, 48.86102],
+  zoom: 15.5
+});
+
+map.addControl(new mapboxgl.NavigationControl());
