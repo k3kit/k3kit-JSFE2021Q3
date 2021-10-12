4622fcf8da10262ba67d4593a6ecbd03c9309705
Nikita berezkin <hadouken7809@gmail.com>
2021-10-12 18:00:39 +0300

    feat: add swiper welcome

--- museum-dom/swiper.js
+++ museum-dom/swiper.js
@@ -51,8 +51,46 @@ const prevSlide = () => {
       })
   })
 
+const swipedetect = (el) => {
+    let surface = el;
+    let startX = 0;
+    let startY = 0;
+    let distX = 0;
+    let distY = 0;
+    
+    let startTime = 0;
+    let elapsedTime = 0;
 
+    let threshold = 150;
+    let restraint = 100;
+    let allowedTime = 300;
 
+    surface.addEventListener('mousedown', function(e){
+        startX = e.pageX;
+        startY = e.pageY;
+        startTime = new Date().getTime();
+        e.preventDefault();
+    })
+    
+    surface.addEventListener('mouseup', function(e){
+        distX = e.pageX - startX;
+        distY = e.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+        if (elapsedTime <= allowedTime) {
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
+                if(distX > 0) { 
+                    prevSlide();
+                } else {
+                    nextSlide();
+                }
+            }
+        }
+        e.preventDefault();
+    })
+}
+
+let el = document.querySelector('.slider__wrapper');
+swipedetect(el);
 
  next.addEventListener('click',nextSlide);
  prev.addEventListener('click',prevSlide);
