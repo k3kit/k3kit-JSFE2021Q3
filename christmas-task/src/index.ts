93260b8cbefcbbe436e3cf07f155dd8dd0dcc6f7
k3kit <hadouken7809@gmail.com>
2022-01-14 14:52:56 +0300

    fix: fix slider

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -1,10 +1,8 @@
 
 import { CardDataModel } from './application/cardDataModel';
 import { ICardData } from './application/cardDataModel';
+import noUiSlider, { target } from 'nouislider'
 import { searchName } from './application/search';
-import { counSlider, yearSlide } from './application/sliderRange';
-// import { counSlider } from './application/sliderRange';
-// import { yearSlide } from './application/sliderRange';
 import { SortSel } from './application/sort';
 import { data } from './data';
 
@@ -112,8 +110,7 @@ const selected: string[] = [];
 
 
 SortSel()
-counSlider()
-yearSlide()
+
 searchName()
 
 const form = document.querySelectorAll('.shape');
@@ -483,3 +480,60 @@ renderTreeSection();
         }
       }
   }
+
+  const  countSlider = <target>document.querySelector('.count-slider');
+const  sliderOutput0 = <HTMLElement>document.querySelector('.slider-output-0');
+const  sliderOutput1 =<HTMLElement>document.querySelector('.slider-output-1');
+const countArray: Array<any> = [sliderOutput0,sliderOutput1]
+
+  function counSlider(){
+  noUiSlider.create(countSlider, {
+    start: [0, 12],
+    connect: true,
+    step: 1,
+    range: {
+        'min': 0,
+        'max': 12
+    }
+});
+countSlider.noUiSlider.on('change',  (values: string[], handle: string | number) => {
+    countArray[handle].innerHTML = parseInt(values[handle]).toString();
+
+      cardContainer.innerHTML = '';
+      const sorted = Array2.filter(function (it) { 
+        return it.count >= parseInt(values[0]) && it.count <= parseInt(values[1])
+      })
+      console.log(sorted);
+      Card(sorted)
+  
+});
+}
+
+  
+  
+const  yearSlider = <target>document.querySelector('.year-slider');
+const  sliderOutput00 = <HTMLElement>document.querySelector('.slider-output-00');
+const  sliderOutput01 =<HTMLElement>document.querySelector('.slider-output-01');
+const yearArray: Array<any> = [sliderOutput00,sliderOutput01]
+ function yearSlide(){
+  noUiSlider.create(yearSlider, {
+    start: [1940, 2020],
+    connect: true,
+    step: 1,
+    range: {
+        'min': 1940,
+        'max': 2020
+    }
+});
+yearSlider.noUiSlider.on('change', function (values: { [x: string]: string; }, handle: string | number) {
+    yearArray[handle].innerHTML = parseInt(values[handle]).toString();
+      cardContainer.innerHTML = '';
+      const sorted = Array2.filter(function (it) { 
+        return it.year >= parseInt(values[0]) && it.year <= parseInt(values[1])
+      })
+      Card(sorted)
+
+});
+}
+yearSlide()
+counSlider()
